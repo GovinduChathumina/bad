822039f56cd7cb2ec85f184df5941684a2204859
GovinduChathumina <46335009+GovinduChathumina@users.noreply.github.com>
2022-12-09 11:00:32 +0530

    Update bad.php

--- bad.php
+++ bad.php
@@ -1,30 +1,11 @@
 <?php
-
-/**
- * Please, improve this class and fix all problems.
- *
- * You can change the Tenant class and its methods and properties as you want.
- * You can't change the AccountingService behavior.
- * You can choose PHP 7 or 8.
- * You can consider this class as an Eloquent model, so you are free to use
- * any Laravel methods and helpers.
- *
- * What is important:
- * - design (extensibility, testability)
- * - code cleanliness, following best practices
- * - consistency
- * - naming
- * - formatting
- *
- * Write your perfect code!
- */
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Tenant extends Model{
     public $accountingService;
+    public int $id;
 
     function __construct(){
         $this->accountingService = new \App\Services\AccountingService();
@@ -32,42 +13,39 @@ class Tenant extends Model{
 
     public function get_invoices()
     {
-        $params = array('tenant_id' => $this->id);
-        $invoices = $this->accountingService->getAllInvoices($params);
+        $tenantIds = array('tenant_id' => $this->id);
+        $invoices = $this->accountingService->getAllInvoices($tenantIds);
         $ap_invoices = array();
         if (!empty($invoices))
         {
             // Loop through all invoices and choose only ones that await payment
-            foreach ($invoices as $i)
+            foreach ($invoices as $invoice)
             {
-                if ($i['status'] == 'awaiting-payment')
-                    $ap_invoices[] = $i;
+                if ($invoice['status'] == 'awaiting-payment')
+                    $ap_invoices[] = $invoice;
             }
             return $ap_invoices;
         }
-
         return null;
     }
 
     public function get_old_invoices()
     {
-        $params = array('tenant_id' => $this->id);
-        $invoices = $this->accountingService->getAllInvoices($params);
+        $tenantIds = array('tenant_id' => $this->id);
+        $invoices = $this->accountingService->getAllInvoices($tenantIds);
 
         if (!empty($invoices)) {
             $paid_invoices = array();
 
             // Loop through all invoices and choose only paid ones
-            foreach ($invoices as $i)
+            foreach ($invoices as $invoice)
             {
-                if ($i['status'] == 'paid') {
-                    $paid_invoices[] = $i;
+                if ($invoice['status'] == 'paid') {
+                    $paid_invoices[] = $invoice;
                 }
+                return $paid_invoices;
             }
-
-            return $paid_invoices;
+            return null;
         }
     }
-
-    // ...
 }
